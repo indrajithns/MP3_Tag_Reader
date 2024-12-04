1c6694f9fc0acc8ab5db780ebc540ed37184c51f
indrajithns <indrajithgowda1998@gmail.com>
2024-12-04 22:22:59 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# MP3 Tag Reader 
+MP3 tag reader is a software which will read and display MP3 (ID3) tag information from MP3 files. The software will be desktop based and not web based. All operations will be performed using command line interface
+
+# Usage 
+### Help Command 
+``` 
+./a.out -h
+```
+it will print all the available commands that can be used with this binary file
+
+
+
+### Print MP3 tag details 
+```
+./a.out <mp3.file>
+./a.out sample.mp3
+```
+
+
+### Modify tag data
+
+To modify title imformation  and view those information 
+```
+./a.out -t "Saidas" sample.mp3 
+./a.out sample.mp3
+```
+
+
+similarly we can use others commands to modify tag information
---
+++ mp3tag.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include<string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "common.h"
+#include "type.h"
+#include "edit.h"
+
+int main(int argc, char * argv[])
+{
+		MP3Info mp3Info;
+		if(argc > 1)
+		{
+				switch(check_operation(argv))
+				{
+						case e_help:
+								print_help();
+								break;
+
+						case e_file:
+								if(find_meta(argc, argv[1], &mp3Info) == e_success)
+										print_tag_data(&mp3Info);
+								break;
+
+						case e_option:
+								if(do_option(argc,argv, &mp3Info) == e_failure)
+								{		print_help();}
+								break;
+						case e_ver:
+								printf("Vesrion 1.1\n");
+								break;
+
+						default:
+								printf("Invalid Option !!!'%s'", argv[1]);
+								print_help();
+								break;
+				}
+
+		}
+		else
+		{
+				printf("Invalid Option !!!\n");
+				print_help();
+		}
+}
